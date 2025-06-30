85d887f2ee157d827e6ce95d42a62f7576f9f1c7
Eliézer da Silva <eliezerdasilva2005@gmail.com>
2025-06-30 19:59:36 -0300

    diagramas

--- record-dot.net/RecordApi/obj/Debug/net9.0/RecordApi.AssemblyInfo.cs
+++ record-dot.net/RecordApi/obj/Debug/net9.0/RecordApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecordApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b6b2d0580dc04c134e0094c7fac5e09ed6bce15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90e6fb31ce9cd3026d27df28d76c0a4179537907")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecordApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecordApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
